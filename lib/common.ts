9a1239cb776365c1c009f5773b6e5f14f0feaecc
Shishin Mo <shishin.mo@reasonwhy.jp>
2021-09-30 23:50:38 +0900

    Delete common, component, lib

--- lib/common.ts
+++
@@ -1,9 +0,0 @@
-import {gan, tiiki} from "../common/hospital";
-
-export const getRegionAndCancerType = (router):[string, string] => {
-    const filename = router.pathname.replace(/^.*\//, "")
-    const pageRegion = tiiki[filename]
-    const cancerName = router.pathname.split("\/")[3]
-    const cancerType = gan[cancerName]
-    return [pageRegion, cancerType]
-}
